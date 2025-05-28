b46100f5e8a283426a1dac2e6781944d141273db
sw4796 <swlee4796@naver.com>
2025-05-28 10:54:40 +0900

    수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AdServingPage from './pages/Adserving/AdServingPage';
+import AdBidPage from './pages/AdBid/AdbidPage';
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/ad-serving" element={<AdServingPage />} />
+        <Route path="/ad-bid" element={<AdBidPage />} />
         {/* ...다른 페이지 */}
       </Routes>
     </BrowserRouter>
---
+++ tailwind.confing.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // 리액트 기준
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
